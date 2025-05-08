0750b7c3b5ee8234a349dcf944101312c4f3a8cd
Hanan Mutlak <hamu2401@student.miun.se>
2025-05-08 15:57:22 +0200

    first commit

--- models/Experience.js
+++ models/Experience.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 
 const experienceSchema = new mongoose.Schema({
-  company: { type: String, required: true },
-  role: { type: String, required: true },
+  company: { type: String,
+     required: true 
+    },
+  role: { type: String,
+     required: true 
+    },
   description: { type: String, required: true },
-  duration: { type: String, required: true }
+  duration: { type: String,
+     required: true 
+    }
 }, {
   timestamps: true
 });
